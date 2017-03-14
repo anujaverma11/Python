81822d1ffdd86769dd00fd35bfc9406f97f6c195
Anuja Verma <anuja.verma@gmail.com>
2017-03-13 23:21:02 -0700

    Intro to CS

---
+++ Introduction_to_CS/hello.py
@@ -0,0 +1,16 @@
+s = "city"
+print s[3:]
+print 'city'.find('y')
+
+
+##########################################################################
+
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+start_link = page.find('<a href=')
+first_quote = page.find('"', start_link) + 1
+second_quote = page.find('"', first_quote)
+url = page[first_quote : second_quote]
+print url
+
+##########################################################################
