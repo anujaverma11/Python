2368dbd4ed85f1081bc033f1ea36f6e5362fac62
Anuja Verma <anuja.verma@gmail.com>
2017-03-15 17:33:52 -0700

    python functions

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Program Name | Examples
 --- | ---
 [take_a_break.py](\take_a_break.py) | functions used time.ctime(), time.sleep() and webbrowser.open
-| | |
+[secret_message.py](\secret_message.py) | functions used os.listdir(), os.getcwd(), os.chdir(), os.rename()
---
+++ secret_message.py
@@ -0,0 +1,13 @@
+import os
+
+def rename_files() :
+  file_list = os.listdir("/Users/anujaverma/Desktop/Nanodegree/NanoDegree/resources/prank")
+  print (file_list)
+  saved_path= os.getcwd()
+  os.chdir("/Users/anujaverma/Desktop/Nanodegree/NanoDegree/resources/prank")
+  for file_name in file_list:
+      print("Old Name - " + file_name)
+      print("new Name - " + file_name.translate(None,"0123456789"))
+      os.rename(file_name, file_name.translate(None,"0123456789"))
+
+rename_files()
