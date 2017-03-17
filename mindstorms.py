69f1f6bbe66ae41e419fe6a88b15c052e3fe9e63
Anuja Verma <anuja.verma@gmail.com>
2017-03-16 20:20:59 -0700

    using turtle

--- mindstorms.py
+++ mindstorms.py
@@ -1,28 +1,30 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+  for i in range(1,5):
+    some_turtle.right(90)
+    some_turtle.forward(100)
+
+def draw_art():
   window = turtle.Screen()
   window.bgcolor("white")
 
+  # draw square
   brad = turtle.Turtle()
   brad.shape("turtle")
   brad.color("purple")
   brad.speed(2)
+  for i in range(1,37):
+    draw_square(brad)
+    brad.right(10)
 
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
 
-  angie = turtle.Turtle()
-  angie.shape("arrow")
-  angie.color("blue")
-  angie.circle(100)
+  # draw circle
+  # angie = turtle.Turtle()
+  # angie.shape("arrow")
+  # angie.color("blue")
+  # angie.circle(100)
 
   window.exitonclick()
 
-draw_square()
+draw_art()
